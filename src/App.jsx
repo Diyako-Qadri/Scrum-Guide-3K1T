4bb6d81beca0f77adf3d74b2e8bce8fa59ccb211
Diyako Qadri <127683357+Diyako-Qadri@users.noreply.github.com>
2024-04-29 15:07:03 +0200

    Revert "Feature/22 navbar"

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,13 @@
 import './App.css'
-import { Route, Routes } from 'react-router-dom'
-import Layout from './pages/Layout'
 
 function App() {
 
 
-    return (
-        <>
-            <Routes>
-                <Route element={<Layout />}>
-                    <Route path='/' element='' />
-                    <Route path='/scrum' element='' />
-                    <Route path='/team' element='' />
-                    <Route path='/events' element='' />
-                    <Route path='/artifacts' element='' />
-                </Route>
-            </Routes>
-        </>
-    )
+  return (
+    <>
+     <p>Hello</p>
+    </>
+  )
 }
 
 export default App
