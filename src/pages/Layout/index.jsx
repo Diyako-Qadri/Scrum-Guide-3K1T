17f0aa11e0721fc4ed80cfd239f75f1463cc4d8d
Darius K <darius.kaya@gmail.com>
2024-04-29 14:58:13 +0200

    navbar done except styling

--- src/pages/Layout/index.jsx
+++ src/pages/Layout/index.jsx
@@ -6,13 +6,16 @@ const Layout = () => {
         <>
             <nav>
                 <div className={styles.navbar}>
-                    <div className={styles.logoContainer}>Logo</div>
+                    <NavLink className={styles.logo} to='/'>Scrum Guide</NavLink>
                     <div className={styles.navItems}>
-                        <NavLink className={styles.navItem} to='/'>Home</NavLink>
                         <NavLink className={styles.navItem} to='/scrum'>Scrum</NavLink>
                         <NavLink className={styles.navItem} to='/team'>The Scrum Team</NavLink>
                         <NavLink className={styles.navItem} to='/events'>Scrum Events</NavLink>
                         <NavLink className={styles.navItem} to='/artifacts'>Scrum Artifacts</NavLink>
+                    </div>
+                    <div className={styles.aboutContact}>
+                        <NavLink className={`${styles.navItem} ${styles.about}`} to='/about'>About</NavLink>
+                        <NavLink className={`${styles.navItem} ${styles.contact}`} to='/contact'>Contact</NavLink>
                     </div>
                 </div>
             </nav>
