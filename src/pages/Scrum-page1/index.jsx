b1df78fcfd36b8b15d75307c515d64fe8f205efc
Diyako-Qadri <qadri.diyako@gmail.com>
2024-04-30 14:18:51 +0200

    created scrum page

---
+++ src/pages/Scrum-page1/index.jsx
@@ -0,0 +1,12 @@
+
+import styles from './Scrum-page1.module.css'
+
+const Scrum-page1 = () => {
+    return (
+       <section className={styles.ScrumPage}>
+
+       </section>
+    )
+};
+
+export default Scrum-page1
