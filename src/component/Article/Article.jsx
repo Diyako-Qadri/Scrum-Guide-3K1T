fc6aac3f629dcd56e8c981a1e4d3cb8b6d8d64df
timdeckel <timdeckel@gmail.com>
2024-05-03 19:07:35 +0200

    made some further styling and made a small change to the home page css

--- src/component/Article/Article.jsx
+++ src/component/Article/Article.jsx
@@ -3,9 +3,24 @@ import styles from "./Article.module.css";
 const Article = () => {
   return (
     <>
-    <div className={styles.article__body}>
-      <h2 className={styles.headline}>Headline</h2>
-      <p className={styles.article__text}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos doloremque voluptatibus vel molestias odit. Tempore doloribus assumenda officia natus, dolorum facere atque! Neque aut placeat adipisci quos, tempora provident ut!</p>
+    <div className={styles.article__container}>
+      <h2 className={styles.article__headline}>Headline</h2>
+      <div className={styles.article__body}>
+        <p className={styles.article__text}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae aliquid sapiente repudiandae accusamus dolorum aspernatur exercitationem libero, debitis ipsum voluptatibus doloribus, autem neque sint iusto blanditiis odit velit! Porro, quo.
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos doloremque voluptatibus vel molestias odit. Tempore doloribus assumenda officia natus, dolorum facere atque! Neque aut placeat adipisci quos, tempora provident ut!
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae aliquid sapiente repudiandae accusamus dolorum aspernatur exercitationem libero, debitis ipsum voluptatibus doloribus, autem neque sint iusto blanditiis odit velit! Porro, quo.
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos doloremque voluptatibus vel molestias odit. Tempore doloribus assumenda officia natus, dolorum facere atque! Neque aut placeat adipisci quos, tempora provident ut!
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae aliquid sapiente repudiandae accusamus dolorum aspernatur exercitationem libero, debitis ipsum voluptatibus doloribus, autem neque sint iusto blanditiis odit velit! Porro, quo.
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos doloremque voluptatibus vel molestias odit. Tempore doloribus assumenda officia natus, dolorum facere atque! Neque aut placeat adipisci quos, tempora provident ut!
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae aliquid sapiente repudiandae accusamus dolorum aspernatur exercitationem libero, debitis ipsum voluptatibus doloribus, autem neque sint iusto blanditiis odit velit! Porro, quo.
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae aliquid sapiente repudiandae accusamus dolorum aspernatur exercitationem libero, debitis ipsum voluptatibus doloribus, autem neque sint iusto blanditiis odit velit! Porro, quo.
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos doloremque voluptatibus vel molestias odit. Tempore doloribus assumenda officia natus, dolorum facere atque! Neque aut placeat adipisci quos, tempora provident ut!
+        </p>
+        <div className={styles.article__image__wrapper}>
+          <div></div>
+        </div>
+      </div>
       <div className={styles.article__details}>
         <p>jane smith</p>
         <p>february 21 2011</p>
