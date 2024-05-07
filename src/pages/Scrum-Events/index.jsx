1fca51e25a0914f37898993962082b20baa0b74c
timdeckel <timdeckel@gmail.com>
2024-05-07 11:41:52 +0200

    merged the article feature branch with the page branch to work on both

--- src/pages/Scrum-Events/index.jsx
+++ src/pages/Scrum-Events/index.jsx
@@ -1,9 +1,15 @@
 import styles from "./Scrum-Events.module.css";
+import Article from "../../component/Article/Article";
 
 const ScrumEvents = () => {
   return(
     <>
-      <div className={styles.scrum__events__structure}></div>
+      <div className={styles.scrum__events__structure}>
+        <div className={styles.article__wrapper}>
+          <Article size="small"/>
+        </div>
+        
+      </div>
     </>
   )
 };
