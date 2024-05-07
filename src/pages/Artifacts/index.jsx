a6ae136c32ddaa9f5b3bd9052d59c7e7ef8c5af6
Darius K <darius.kaya@gmail.com>
2024-05-07 13:53:38 +0200

    pit stop

--- src/pages/Artifacts/index.jsx
+++ src/pages/Artifacts/index.jsx
@@ -1,10 +1,45 @@
 import styles from './Artifacts.module.css'
+import { motion } from 'framer-motion'
 
 const Artifacts = () => {
     return (
-        <div>
-            <h1>HELLO</h1>
-        </div>
+        <>
+            <div className={styles.artifactsBg}>
+                <div className={styles.headlineContainer}>
+                    <motion.h1
+                        initial={{
+                            opacity: 0,
+                            y: 150,
+                        }}
+                        whileInView={{
+                            opacity: 1,
+                            y: 0,
+                            transition: {
+                                duration: 2
+                            }
+                        }}
+                        viewport={{ once: false }}>
+                        Scrum <motion.span
+                            initial={{
+                                opacity: 0,
+                                transition: {
+                                    delay: 2 // Delay of 2 seconds
+                                }
+                            }}
+                            whileInView={{
+                                opacity: 1,
+                                transition: {
+                                    duration: 2
+                                }
+                            }}
+                            viewport={{ once: false }}
+                            className={styles.underline}>
+                            Artifacts
+                        </motion.span> 
+                    </motion.h1>
+                </div>
+            </div>
+        </>
     )
 };
 
